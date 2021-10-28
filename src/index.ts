0e89b2db35eca100610cf69d0a7de3039f29d9aa
shon0 <shono.koga@gmail.com>
2021-10-28 21:27:19 +0900

    generates types for TS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import {ApolloServer} from 'apollo-server';
+import {ApolloServer, AuthenticationError} from 'apollo-server';
 import {loadSchemaSync} from '@graphql-tools/load';
 import {GraphQLFileLoader} from '@graphql-tools/graphql-file-loader';
 import {addResolversToSchema} from '@graphql-tools/schema';
 import {join} from 'path';
+import {Resolvers} from './types/generated/graphql';
+import {Context} from './types/context';
 
 // sample data
 const books = [
@@ -20,15 +22,39 @@ const schema = loadSchemaSync(join(__dirname, '../schema.graphql'), {
   loaders: [new GraphQLFileLoader()],
 });
 
-const resolvers = {
+const resolvers: Resolvers = {
   Query: {
-    books: () => books,
+    books: (_parent, _args, _context) => {
+      return books;
+    },
   },
 };
 
 const schemaWithResolver = addResolversToSchema({schema, resolvers});
 
-const server = new ApolloServer({schema: schemaWithResolver});
+const getUser = (token?: string): Context['user'] => {
+  if (!token) {
+    throw new AuthenticationError(
+      '認証されていないユーザーはリソースにアクセスできません'
+    );
+  }
+
+  return {
+    name: 'dummy name',
+    email: 'dummy@example.com',
+    token,
+  };
+};
+
+const server = new ApolloServer({
+  schema: schemaWithResolver,
+  context: ({req}) => {
+    if (req.body.operationName === 'IntrospectionQuery') return {};
+    return {user: getUser(req.headers.authorization)};
+  },
+  debug: process.env.NODE_ENV === 'development',
+  introspection: true,
+});
 
 server.listen().then(({url}) => {
   console.log(`🚀 Server ready at ${url}`);
